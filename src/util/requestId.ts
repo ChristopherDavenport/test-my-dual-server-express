7a14540401f340af43caba15f439ef58a784dfe4
Christopher Davenport <chris@christopherdavenport.tech>
2021-10-30 13:51:38 -0700

    Fix RequestId Middleware

--- src/util/requestId.ts
+++ src/util/requestId.ts
@@ -3,11 +3,11 @@ import {v4 as uuidv4} from "uuid" // Assumption of default export can end in uni
 // Uncertain how to tell ahead of time, but this explains things I've found a couple times now.
 
 /**
- * Add A Unique Request Id to each incomming request. 
+ * Add A Unique Request Id to each incomming request.
  */
 const middleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
-    const id = req.headers['X-Request-Id'] || req.headers['x-request-id'] || uuidv4().replace(/-/g, '')
+    const id: RequestId = req.header('X-Request-Id') || uuidv4().replace(/-/g, '')
     res.locals.requestId = id
     res.setHeader("X-Request-Id", id)
     next()
