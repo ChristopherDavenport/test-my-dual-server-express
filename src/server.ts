a551193a0ede4663ca5d6525297a86f94b2810be
Christopher Davenport <chris@christopherdavenport.tech>
2021-10-30 10:50:21 -0700

    Attempt to Flush Jaeger Tracer

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,29 @@
 import express from "express"
-import http from 'http';
+import http, { Server } from 'http';
 import adminApp from './routes/admin/admin'
 import tracing from './util/tracing';
 import httpApp from './routes/http/http'
 import configs from "./config/configs";
 import { Tracer } from "opentracing";
+import { JaegerTracer } from "jaeger-client";
 
 // Helper Functions
 const onListen = (server: http.Server, port: number, desc: string) => {
   server.on("listening", () => {
     console.log(desc + " Server started at: " + port)
+  })
+}
+
+const shutdownServers = (httpServer: http.Server, adminServer: http.Server, tracer: JaegerTracer) => {
+  process.on('SIGTERM', () => {
+    console.log('Termination Sent')
+    adminServer.close(() => {
+      console.log('Admin server closed')
+    })
+    httpServer.close(() => {
+      console.log('HTTP server closed')
+      tracer.close()
+    })
   })
 }
 
@@ -30,6 +44,8 @@ const runServer = () => {
   const adminServer = http.createServer(adminApp.buildApp(adminApplication, tracer))
   adminServer.listen(config.admin.port)
   onListen(adminServer, config.admin.port, config.admin.description)
+
+  shutdownServers(httpServer, adminServer, tracer)
 }
 
 export default {
